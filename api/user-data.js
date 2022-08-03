691e08326c726a7913d3588a9bb9ff395bcd74fb
Isaac Newton Tingo <newtone480@gmail.com>
2022-08-03 15:54:00 +0300

    Change request type

--- api/user-data.js
+++ api/user-data.js
@@ -2,7 +2,7 @@ const express = require("express");
 const User = require("../models/user");
 const router = express.Router();
 
-router.get("/account-balance", async (req, res) => {
+router.post("/account-balance", async (req, res) => {
   const { phoneNumber } = req.body;
 
   if (!phoneNumber) {
